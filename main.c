91db6d657bd34e1fb064e146a321a60900a00ea1
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2023-11-29 15:56:01 -0500

    Primer commit realizado junto declaraciones y librerias

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    srand (time(NULL));
+    int a,b;
+    printf("Ingrese el numero de filas de la matriz\n");
+    scanf("%d", &a);
+    printf("Ingrese el numero de columnas de la matriz\n");
+    scanf("%d", &b);
+    printf("Matriz 1\n");
+    int matriz[a][b];
+    for(int i = 0; i < a; i++) {
+        for(int j = 0; j < b; j++) {
+            matriz[i][j] = rand() % 101; 
+            printf("%d\t", matriz[i][j]); 
+        }
+        printf("\n"); 
+    }
+    printf("Matriz 2\n");
+    int matriz2[a][b];
+    for(int i = 0; i < a; i++) {
+        for(int j = 0; j < b; j++) {
+            matriz2[i][j] = rand() % 101; 
+            printf("%d\t", matriz2[i][j]); 
+        }
+        printf("\n"); 
+    }
+    printf("Suma de las matrices\n");
+    int suma[a][b];
+    for(int i = 0; i < a; i++) {
+        for(int j = 0; j < b; j++) {
+            suma[i][j] = matriz[i][j] + matriz2[i][j];
+            printf("%d\t", suma[i][j]);
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
