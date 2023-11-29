f84017b5dc15e55d0636caf2a5c0b971f4bbb5c8
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2023-11-29 15:57:49 -0500

    Asignacion de numeros a las matrices

--- main.c
+++ main.c
@@ -6,15 +6,15 @@ int main(int argc, char const *argv[])
 {
     srand (time(NULL));
     int a,b;
-    printf("Ingrese el numero de filas de la matriz\n");
+    printf("Ingrese el numero de filas de la matriz\n"); //Solicitud de numeros de filas 
     scanf("%d", &a);
-    printf("Ingrese el numero de columnas de la matriz\n");
+    printf("Ingrese el numero de columnas de la matriz\n"); //Solicitud de numeros de columnas
     scanf("%d", &b);
     printf("Matriz 1\n");
     int matriz[a][b];
     for(int i = 0; i < a; i++) {
         for(int j = 0; j < b; j++) {
-            matriz[i][j] = rand() % 101; 
+            matriz[i][j] = rand() % 101; //Asignacion de numeros dentro de la matriz
             printf("%d\t", matriz[i][j]); 
         }
         printf("\n"); 
@@ -24,19 +24,13 @@ int main(int argc, char const *argv[])
     for(int i = 0; i < a; i++) {
         for(int j = 0; j < b; j++) {
             matriz2[i][j] = rand() % 101; 
-            printf("%d\t", matriz2[i][j]); 
+            printf("%d\t", matriz2[i][j]); //Se asigna e imprime la segunda matriz con los numeros asignados 
         }
         printf("\n"); 
     }
     printf("Suma de las matrices\n");
     int suma[a][b];
-    for(int i = 0; i < a; i++) {
-        for(int j = 0; j < b; j++) {
-            suma[i][j] = matriz[i][j] + matriz2[i][j];
-            printf("%d\t", suma[i][j]);
-        }
-        printf("\n");
-    }
+    
     
 
     return 0;
